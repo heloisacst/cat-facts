a58938e99f2357a1033a5d750a9c22f89118639d
Heloisa Cristina dos Santos <heloisa.santos@zitrus.com.br>
2023-05-15 17:42:31 -0300

    first commit

---
+++ src/components/Facts.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <ul>
+      <li class="list" v-for="(item, index) in dados" :key="item.id">
+        <div class="card">
+          <img class="pics" :src="pics[index].url" :alt="pics[index].id">
+          <div class="text">{{ item.text }}</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'FactsText',
+
+  data() {
+      return {
+        dados: [],
+        pics: []
+      }
+    },
+
+    methods: {
+      async getData() {
+        try {
+            const response = await axios.get('https://cat-fact.herokuapp.com/facts');
+            //console.log(response.data); // Verifica a resposta da API
+            this.dados = response.data;
+        } catch (error) {
+            console.log(error);
+        }
+      },
+
+      async getPic() {
+        try {
+            const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=10');
+            const images = response.data.slice(0, 5);
+            console.log(response.data); // Verifica a resposta da API
+            this.pics = images;
+        } catch (error) {
+            console.log(error);
+        }
+      }
+    },
+
+    created() {
+      this.getData();
+      this.getPic();
+    }
+}
+</script>
+
+<style scoped>
+
+ul {
+  display: flex;
+  flex-direction: row;
+}
+
+.card {
+  background-color: #222222;
+  color: #F3EFE0;
+  width: 20rem;
+  height: 20rem;
+  margin: 10px;
+  border-radius: 1rem;
+  border: 1px solid black;
+  margin-bottom: 1em;
+  padding: 1em;
+}
+
+.text {
+  margin: 0.5rem 0;
+}
+
+.list {
+  gap: 1rem;
+  list-style-type: none;
+  font-size: 0.8rem;
+}
+
+.pics {
+  max-width: 100%;
+  max-height: 100%;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Facts/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Facts from '@/components/Facts.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Facts
   }
 }
 </script>
